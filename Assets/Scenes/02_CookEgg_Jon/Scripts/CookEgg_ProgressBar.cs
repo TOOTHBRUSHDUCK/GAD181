b7199c3b4248e5a7077480374f3c795513696cb0
Jon Wong <jonwong82@gmail.com>
2022-10-18 23:37:57 +0800

    Updated Egg Color change and Progress Bar Color change (Code)

--- Assets/Scenes/02_CookEgg_Jon/Scripts/CookEgg_ProgressBar.cs
+++ Assets/Scenes/02_CookEgg_Jon/Scripts/CookEgg_ProgressBar.cs
@@ -11,6 +11,7 @@ public class CookEgg_ProgressBar : MonoBehaviour
     [SerializeField] private Slider temperatureSlider; 
     [SerializeField] private Slider progressSlider;
     [SerializeField] private TextMeshProUGUI sliderText;
+    public bool progressOn = false;
     public float minTempValue;
     public float maxTempValue;
 
@@ -23,19 +24,46 @@ public class CookEgg_ProgressBar : MonoBehaviour
         );
 
         InvokeRepeating("ProgressChange", 1.0f, progressIncreaseRate);
+        ProgressTurnOn();
     }
 
     
     void Update()
     {
         ProgressChange();
+        ProgressColorChange();
     }
 
     private void ProgressChange()
     {
-        if(temperatureSlider.value > minTempValue && temperatureSlider.value <maxTempValue)
+        if(temperatureSlider.value > minTempValue && temperatureSlider.value <maxTempValue && progressOn == true)
         {
             progressSlider.value += progressIncreaseValue; 
         }
     }
+
+    private void ProgressColorChange()
+    {
+        if(progressSlider.value < 30 && progressOn)
+        {
+            sliderText.color = Color.red;
+        }
+        else if(progressSlider.value >= 30 && progressSlider.value <70)
+        {
+            sliderText.color = Color.blue;
+        }
+        else if(progressSlider.value >= 70)
+        {
+            sliderText.color = Color.green;
+        }
+    }
+
+    public void ProgressTurnOn()
+    {
+        progressOn = true;
+    }
+    public void ProgressTurnOff()
+    {
+        progressOn = false;
+    }    
 }
