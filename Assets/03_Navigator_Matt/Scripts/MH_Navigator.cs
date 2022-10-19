a696c18e47e78794529fcc684b10761a21ba7bb3
Matthew Hopkins <1027615@student.sae.edu.au>
2022-10-19 11:56:18 +0800

    Finished greybox
    Got navigator to move,  caused a win/fail state

---
+++ Assets/03_Navigator_Matt/Scripts/MH_Navigator.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MH_Navigator : MonoBehaviour
+{
+    // this is the players move speed
+    public float moveSpeed = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Move player via WASD keys
+        if (Input.GetKey(KeyCode.W) == true) { this.transform.position += this.transform.forward * moveSpeed * Time.deltaTime; }
+        if (Input.GetKey(KeyCode.S) == true) { this.transform.position -= this.transform.forward * moveSpeed * Time.deltaTime; }
+
+        if (Input.GetKey(KeyCode.D) == true) { this.transform.position += this.transform.right * moveSpeed * Time.deltaTime; }
+        if (Input.GetKey(KeyCode.A) == true) { this.transform.position -= this.transform.right * moveSpeed * Time.deltaTime; } 
+    }
+
+        // Calls the quit game function when the player makes contact with either of the two walls
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(collision.CompareTag("Wall"))
+        {
+            // Quit the game
+            Debug.Log("Fail");
+            QuitGame();
+        }
+        else if (collision.CompareTag("Green Wall"))
+        {
+            Debug.Log("Win");
+            QuitGame();
+        }
+    }
+     //Call this to quit the game when in the editor (NOTE: this will not work if you build the executable)
+    void QuitGame() 
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+}
